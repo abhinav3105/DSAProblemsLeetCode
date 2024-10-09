d6347fffe2f676347bdf6307ccf4d4701d67f8b7
Abhinav Dubey <106994648+abhinav3105@users.noreply.github.com>
2024-10-09 17:30:49 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (79.36%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int o = 0, c = 0;
+        for (char ch : s.toCharArray()) {
+            if (ch == '(') {
+                o++;
+            } else if (o > 0) {
+                o--;
+            } else {
+                c++;
+            }
+        }
+        return o + c;
+    }
+}
