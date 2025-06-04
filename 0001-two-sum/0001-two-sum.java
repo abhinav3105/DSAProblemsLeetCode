a0da718788a051cc5b25326ca68964f2deb4305a
Abhinav Dubey <106994648+abhinav3105@users.noreply.github.com>
2025-06-04 15:10:13 +0530

    Time: 3 ms (60.72%), Space: 45.3 MB (18.96%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,13 +1,21 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int arr[] = {-1,-1};
+        HashSet<Integer> set = new HashSet<>();
+        int[] arr = {-1,-1};
+        int x = -1;
+        set.add(nums[0]);
+        for(int i = 1;i<nums.length;i++){
+            x = target-nums[i];
+            if(set.contains(x)){
+                arr[1] = i;
+                break;
+            }
+            set.add(nums[i]);
+        }
         for(int i = 0;i<nums.length;i++){
-            for(int j = i+1;j<nums.length;j++){
-                if(nums[i]+nums[j]== target){
-                    arr[0]= i;
-                    arr[1]= j;
-                    return arr;
-                }
+            if(nums[i]==x){
+                arr[0] = i;
+                break;
             }
         }
         return arr;
